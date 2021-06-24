b9b8fcb9e38c35064d3d7b4e76a9a80cdc4e61a8
prexlab <37144853+prexlab@users.noreply.github.com>
2021-06-24 20:54:32 +0900

    Update scroll-trigger.js

--- scroll-trigger.js
+++ scroll-trigger.js
@@ -1,15 +1,9 @@
 var getScrollTrigger = function (selector, callback) {
-
     return function () {
         let scroll = $(window).scrollTop();
         let windowHeight = window.innerHeight ? window.innerHeight: $(window).height();
-
         $(selector).each(function () {
-
             var position = $(this).offset().top;
-
-            console.log([position, scroll, windowHeight, scroll + windowHeight * 3 / 4]);
-
             if ((scroll + windowHeight * 3 / 4 > position) && !$(this).is('.scrolled')) {
                 $(this).addClass('scrolled');
                 callback(this);
